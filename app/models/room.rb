19acf94293f52b15961561b6720575b50b0cb4c4
Hiroki Yanase <60053865+YanaseHiroki@users.noreply.github.com>
2020-07-22 15:33:19 +0900

    チャットルーム作成機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,7 @@ class Room < ApplicationRecord
   has_many :room_users
   # 1つのチャットルームに2人のユーザー
   has_many :users, through: :room_users
+
+  # ルーム名が存在（presence）している場合のみ作成可（true）
+  validates :name, presence: true
 end
