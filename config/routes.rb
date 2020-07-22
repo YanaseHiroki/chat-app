459cdf55f1c69c453f10ab33f3fd7a50b10002c5
Hiroki Yanase <60053865+YanaseHiroki@users.noreply.github.com>
2020-07-22 11:04:30 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root "messages#index"
   # ユーザー編集に必要なルーティングは、editとupdateなので、
   resources :users, only: [:edit, :update]
+  # 新規チャットルームの作成で動くアクションは「new」と「create」のみ
+  resources :rooms, only: [:new, :create]
 end
