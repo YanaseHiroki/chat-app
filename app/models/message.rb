8447b5bc9f20783179463deb2caaa7245e11f20a
Hiroki Yanase <60053865+YanaseHiroki@users.noreply.github.com>
2020-07-23 16:06:01 +0900

    空のメッセージが送信できないバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,6 @@ class Message < ApplicationRecord
   # 1つの投稿に1人のユーザー
   belongs_to :user
   
-  # メッセージが存在（presence）している場合のみ作成可（true）
+  # 投稿にメッセージが存在（presence）している場合のみ作成可（true）
   validates :content, presence: true
 end
